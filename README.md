e24508b711820c90deeb5b8937f5baebd65c6ff1
BoumedineRayan <151127114+BoumedineRayan@users.noreply.github.com>
2025-11-25 20:06:53 +0100

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-here is the live demo :  https://boumedinerayan.github.io/VueJsProject/
--- data/marineData.js
+++ data/marineData.js
@@ -5,30 +5,30 @@ const marineAnimals = [
         title: "Clownfish",
         def: "Colorful fish found in coral reefs. They have a symbiotic relationship with sea anemones.",
         img: "../assets/Clownfish.jpg",
-        infoPage: "info/clownfish.html"
+        infoPage: "../info/clownfish.html"
     },
     {
         title: "Dolphin",
         def: "Intelligent marine mammal known for its playful behavior and social nature.",
         img: "../assets/Dolphin.jpg",
-        infoPage: "info/dolphin.html"
+        infoPage: "../info/dolphin.html"
     },
     {
         title: "Sea Turtle",
         def: "Reptiles of the ocean that can live for decades, known for their long migrations.",
         img: "../assets/Turtle.jpg",
-        infoPage: "info/sea-turtle.html"
+        infoPage: "../info/sea-turtle.html"
     },
     {
         title: "Great White Shark",
         def: "A large predatory fish known for its size and power, often found in coastal surface waters.",
         img: "../assets/great_white_shark.jpg",
-        infoPage: "info/great_white_shark.html"
+        infoPage: "../info/great_white_shark.html"
     },
     {
         title: "Blue Whale",
         def: "The largest animal known to have ever existed, blue whales are massive marine mammals.",
         img: "../assets/blue_whale.jpg",
-        infoPage: "info/blue_whale.html"
+        infoPage: "../info/blue_whale.html"
     }
 ];
